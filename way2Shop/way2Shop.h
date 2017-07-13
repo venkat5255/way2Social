5fba8daf54782225dc63d79b4f386f60b515ac09
Kartheek <kartheek528@gmail.com>
2017-07-13 18:06:35 +0530

    Initial Commit

---
+++ way2Shop/way2Shop.h
@@ -0,0 +1,19 @@
+//
+//  way2Shop.h
+//  way2Shop
+//
+//  Created by Venkata rao on 13/07/17.
+//  Copyright © 2017 Way2Online. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for way2Shop.
+FOUNDATION_EXPORT double way2ShopVersionNumber;
+
+//! Project version string for way2Shop.
+FOUNDATION_EXPORT const unsigned char way2ShopVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <way2Shop/PublicHeader.h>
+
+
